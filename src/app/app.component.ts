f26bd3efe638216e0f017aa2b7100772e1e462f9
Laurie <lauriejennifer@gmail.com>
2020-09-22 19:27:42 -0500

    Corrected the search feature TS code, but search feature not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   sourceList: Satellite[];
   displayList: Satellite[];
   constructor() {
+    this.displayList = [];
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
@@ -25,8 +26,20 @@ export class AppComponent {
           this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
- 
  }
+ search(searchTerm: string): void {
+   let matchingSatellites: Satellite[] = [];
+   searchTerm = searchTerm.toLowerCase();
+   for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+      }
+   }
+   // assign this.displayList to be the array of matching satellites
+   // this will cause Angular to re-make the table, but now only containing matches
+   this.displayList = matchingSatellites;
+}
 }
   
   /*The OG constructor() {
